9b8870e0090d7e619cfe0cf8b445c206a2f3181e
LinekerAA <lineker_aguiar@hotmail.com>
2021-03-20 16:41:54 -0300

    Adicionando verificação dos algoritmos escolhidos para resolução

--- tp.c
+++ tp.c
@@ -1,4 +1,7 @@
 #include "./algorithm/brute_force.h"
+#include<string.h>
+#include<stdbool.h>
+
 
 void ler_pesos(int *n, int *pesos)
 {
@@ -13,22 +16,53 @@ int main(int numargs, char *args[])
 {
   int t, i = 0;
   int n, k, solution, *pesos;
+  bool PD = false, AG = false, FB = false;
 
   scanf("%d", &t);
 
+
+  for(i=1; i < numargs; i++){
+    if( strcmp(args[i], "FB") ){
+      FB = true;
+    }
+    
+    else if ( strcmp(args[i], "PD") ){
+      PD = true;
+    }
+
+    else if ( strcmp(args[i], "AG") ){
+      AG = true;
+    }
+  }
+
   do
   {
+    
     scanf("%d %d", &n, &k);
 
     pesos = (int *)calloc(n + 1, sizeof(int));
 
     ler_pesos(&n, pesos);
 
-    solution = brute_force(n, k, pesos);
-    printf("%d\n", solution);
+    if( FB )
+    {
+      solution = brute_force(n, k, pesos);
+      printf("%d\n", solution);
+    }
+    else if ( PD )
+    {
+      /* code */
+    }
+    else if ( AG )
+    {
+      /* code */
+    }
+    
+    
     i = i + 1;
 
     free(pesos);
+    
 
   } while (i < t);
 
