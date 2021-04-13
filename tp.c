31dc53f4ee0cedf2a40c733cc74a7e18b584c3f6
jjader03 <jjader03@gmail.com>
2021-04-13 17:05:30 -0300

    corrigindo o algoritmo de execução de tempo

--- tp.c
+++ tp.c
@@ -4,16 +4,16 @@
 #include "./libraries/input_function.h"
 #include <time.h>
 
-
 int main(int numargs, char *args[])
 {
   int t, i = 0;
   int n, k, solution, *pesos;
   clock_t tempo;
+  double tempo_gasto;
 
   scanf("%d", &t);
 
-  //int algorithm = choose_algorithm(numargs, args);
+  int algorithm = choose_algorithm(numargs, args);
 
   do
   {
@@ -23,27 +23,32 @@ int main(int numargs, char *args[])
 
     read_path(n, pesos);
 
-    // switch (algorithm)
-    // {
-    // case 1:
-    tempo = clock();
-    solution = brute_force(n, k, pesos);
-    tempo = clock() - tempo;
-    double tempo_gasto = (double)(tempo) * 1000/ CLOCKS_PER_SEC;
-    printf("%d %d %d %f \n", n, k, solution, tempo_gasto);
-    //   break;
+    switch (algorithm)
+    {
+    case 1:
+      tempo = clock();
+      solution = brute_force(n, k, pesos);
+      tempo = clock() - tempo;
+      tempo_gasto = (double)(tempo)*1000 / CLOCKS_PER_SEC;
+      printf("%d %d %d %f \n", n, k, solution, tempo_gasto);
+      break;
 
-    // case 2:
-    //   solution = guloso(n, k, pesos);
-    //   printf("%d\n", solution);
-    //   break;
+    case 2:
+      tempo = clock();
+      solution = guloso(n, k, pesos);
+      tempo = clock() - tempo;
+      tempo_gasto = (double)(tempo)*1000 / CLOCKS_PER_SEC;
+      printf("%d %d %d %f \n", n, k, solution, tempo_gasto);
+      break;
 
-    // case 3:
-    //   solution = dynamic_algorithm(n + 1, n, k, pesos);
-    //   printf("%d\n", solution);
-
-    //   break;
-    // }
+    case 3:
+      tempo = clock();
+      solution = dynamic_algorithm(n + 1, n, k, pesos);
+      tempo = clock() - tempo;
+      tempo_gasto = (double)(tempo)*1000 / CLOCKS_PER_SEC;
+      printf("%d %d %d %f \n", n, k, solution, tempo_gasto);
+      break;
+    }
 
     i = i + 1;
     free(pesos);
